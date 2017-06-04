8f7c582fdc8db38961b7c461f9e4fb170b19fee1
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-06-05 00:40:43 +0700

    util.js helper done

---
+++ helpers/util.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const secret = process.env.TOKEN_SECRET;
+var jwt = require('jsonwebtoken')
+
+methods.userInfo = (token, callback) => {
+  if(token){
+    jwt.verify(token, secret, (err, decoded) => {
+      if(decoded){
+        callback(decoded)
+      } else {
+        return 'No Information'
+      }
+    })
+  } else {
+    return 'No Access'
+  }
+}
+
+module.exports = methods
